94099ac66c95a3b206546dbbae13fe56454d6e16
Raoul1996 <baobaozhou199606@gmail.com>
2017-08-30 09:31:49 +0800

    ch2-3 complete

--- code/utils/index.js
+++ code/utils/index.js
@@ -3,13 +3,24 @@ import Promise from '../lib/promise'
 import config from './config'
 import * as Mock from './mock'
 
+const DEFAULT_REQUEST_OPTIONS = {
+  url: '',
+  data: {},
+  header: {
+    'Content-Type': 'application/json'
+  },
+  method: 'GET',
+  dataType: 'json'
+}
+
+// util 工具类
 let util = {
   isDEV: config.isDev,
-  log(){
+  log() {
     this.isDEV && console.log(...arguments)
   },
-  alert(title = '提示', content = config.defaultAlertMsg){
-    if('object' === typeof content){
+  alert(title = 'notice', content = config.defaultAlertMsg) {
+    if ('object' === typeof content) {
       content = this.isDEV && JSON.stringify(content) || config.defaultAlertMsg
     }
     wx.showModal({
@@ -17,27 +28,68 @@ let util = {
       content
     })
   },
-  getStorageData(key, cb){
+  getStorageData(key, cb) {
     let self = this
     wx.getStorage({
       key,
-      success(res){
+      success(res) {
         cb && cb(res.data)
       },
-      fail(err){
+      fail(err) {
         let msg = err.errMsg || ''
-        if( /getStorage:fail/.test(msg) ){
+        if (/getStorage:fail/.test(msg)) {
           self.setStorageData(key)
         }
       }
     })
   },
-  setStorageData(key, value = '', cb){
+  setStorageData(key, value = '', cb) {
     wx.setStorage({
       key,
       data: value,
-      success(){
+      success() {
         cb && cb()
+      }
+    })
+  },
+  request(opt) {
+    let options = Object.assign({}, DEFAULT_REQUEST_OPTIONS, opt)
+    let {url, data, header, method, dataType, mock = false} = opt
+    let self = this
+    // wo~_~,回调地狱
+    return new Promise((resolve, reject) => {
+      if (mock) {
+        let res = {
+          statusCode: 200,
+          data: Mock[url]
+        }
+        if (res && res.statusCode === 200 && res.data) {
+          resolve(res.data)
+        } else {
+          self.alert('notice', res)
+          reject(res)
+        }
+      } else {
+        wx.request({
+          url: url,
+          data: data,
+          header: header,
+          method: method,
+          dataType: dataType,
+          success: function (res) {
+            if (res && res.statusCode === 200 && res.data) {
+              resolve(res.data)
+            } else {
+              self.alert('notice', res)
+              reject(res)
+            }
+          },
+          fail: function (err) {
+            self.log(err)
+            self.alert('notice', err)
+            reject(err)
+          }
+        })
       }
     })
   }
